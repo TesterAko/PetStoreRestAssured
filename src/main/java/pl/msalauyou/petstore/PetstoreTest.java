8473e5e32153bc6019978a1b654236d8e2fa917c
Mikalai Salauyou <ec3@qad.com>
2023-09-21 19:19:07 +0200

    Add new tests and possibility to run in cmd

--- src/main/java/pl/msalauyou/petstore/PetstoreTest.java
+++ src/main/java/pl/msalauyou/petstore/PetstoreTest.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+
 /**
  * Hello world!
  */
@@ -36,6 +38,48 @@ public class PetstoreTest {
                         """)
                 .when().post()
                 .then()
+                .statusCode(200)
+                .body("id", equalTo(22953))
+                .body("name", equalTo("Al"))
+                .body("status", equalTo("available"));
+    }
+
+    @Test
+    public void retrievePet() {
+        RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2/pet")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body("""
+                        {
+                            "id": 22953,
+                            "category": {
+                                "id": 0,
+                                "name": "string"
+                            },
+                            "name": "Al",
+                            "photoUrls": [
+                                "http://placeimg.com/640/480"
+                            ],
+                            "tags": [
+                                {
+                                    "id": 0,
+                                    "name": "string"
+                                }
+                            ],
+                            "status": "available"
+                        }
+                        """)
+                .when().post()
+                .then()
                 .statusCode(200);
+
+        RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2/pet")
+                .when().get("22953")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(22953))
+                .body("name", equalTo("Al"))
+                .body("status", equalTo("available"));
     }
 }
