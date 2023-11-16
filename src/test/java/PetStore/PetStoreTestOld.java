ec57a057d12dc54fb872d08bab87c3f351fd9cd6
TesterAko <e.gdk1992@gmail.com>
2023-11-16 17:17:15 +0100

    actual for senior

---
+++ src/test/java/PetStore/PetStoreTestOld.java
@@ -0,0 +1,127 @@
+package PetStore;
+
+import io.restassured.RestAssured;
+import org.apache.http.HttpHeaders;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeMethod;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class PetStoreTestOld {
+
+
+    /*@Before
+    public void setup() {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
+    }*/
+
+    @Test
+    public void testPetStoreCreate() {
+        RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2/pet")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body("""
+                        {
+                            "id": 12345,
+                            "category": {
+                                "id": 0,
+                                "name": "string"
+                            },
+                            "name": "Al",
+                            "photoUrls": [
+                                "http://placeimg.com/640/480"
+                            ],
+                            "tags": [
+                                {
+                                    "id": 0,
+                                    "name": "string"
+                                }
+                            ],
+                            "status": "available"
+                        }
+                        """)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(12345))
+                .body("name", equalTo("Al"))
+                .body("status", equalTo("available"));
+    }
+    /*
+      @Test
+    void createPet(){
+        RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2/pet")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body("""
+                        {
+                            "id": 22953,
+                            "category": {
+                                "id": 0,
+                                "name": "string"
+                            },
+                            "name": "Al",
+                            "photoUrls": [
+                                "http://placeimg.com/640/480"
+                            ],
+                            "tags": [
+                                {
+                                    "id": 0,
+                                    "name": "string"
+                                }
+                            ],
+                            "status": "available"
+                        }
+                        """)
+                .when().post()
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(22953))
+                .body("name", equalTo("Al"))
+                .body("status", equalTo("available"));
+    }
+
+
+    @Test
+    public void retrievePet() {
+        RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2/pet")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body("""
+                        {
+                            "id": 22953,
+                            "category": {
+                                "id": 0,
+                                "name": "string"
+                            },
+                            "name": "Al",
+                            "photoUrls": [
+                                "http://placeimg.com/640/480"
+                            ],
+                            "tags": [
+                                {
+                                    "id": 0,
+                                    "name": "string"
+                                }
+                            ],
+                            "status": "available"
+                        }
+                        """)
+                .when().post()
+                .then()
+                .statusCode(200);
+
+        RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2/pet")
+                .when().get("22953")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(22953))
+                .body("name", equalTo("Al"))
+                .body("status", equalTo("available"));
+    }
+}
+     */
+}
